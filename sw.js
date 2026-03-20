1524d7186922c819b2fc0aaa6d1dc1787dfb0d6d
jeremyleduc59-byte <jeremy.leduc59@gmail.com>
2026-03-20 17:15:14 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,19 +1,22 @@
-const CACHE_NAME = 'qr-v1';
+
+const CACHE_NAME = 'qr-qty-v1';
 const ASSETS = [
   'index.html',
   'manifest.json',
-  'https://unpkg.com/@zxing/library@latest',
+  'https://unpkg.com/@zxing/library@latest/umd/index.min.js',
   'https://cdn.jsdelivr.net/npm/bwip-js@3/dist/bwip-js-min.js'
 ];
 
-self.addEventListener('install', (e) => {
-  e.waitUntil(
+// Installation du Service Worker
+self.addEventListener('install', (event) => {
+  event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((res) => res || fetch(e.request))
+// Stratégie de mise en cache : Réseau puis Cache
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
   );
 });
